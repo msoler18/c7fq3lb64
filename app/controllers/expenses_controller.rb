538f5fe456a6d7679b2d92bd9666179167335826
Miguel Soler <msoler735@gmail.com>
2017-05-30 23:18:39 -0500

    Add functions expenses for user

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]	
   def index
-     @expenses = Expense.where("concept like? AND category_id like?","%#{params[:concept]}%","%#{params[:category_id]}%")
+     @expenses = current_user.expenses
+     #@search = Expense.where("concept like? AND category_id like?","%#{params[:concept]}%","%#{params[:category_id]}%")
   end
 end
